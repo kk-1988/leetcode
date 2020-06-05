8cd2fc9b8d891420429c3d0eeef8d853ddc21c51
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-06-05 09:30:41 +0800

    ltc 7 官方算法

--- ltc_7.c
+++ ltc_7.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#if 0
 int reverse(int x){
     long result = 0, y = x;
     int min = 0x80000000;
@@ -14,6 +16,28 @@ int reverse(int x){
     
     return (result > max || result < min) ? 0:result;
 }
+
+#else
+#include <limits.h>
+int reverse(int x){
+    int rev = 0;
+    printf("max = %d\r\n",INT_MAX);
+    printf("min = %d\r\n",INT_MIN);
+    
+    while (x != 0) {
+        int pop = x % 10;
+        printf("pop = %d ",pop);
+        x /= 10;
+        printf("x = %d ",x);
+        printf("rev = %d\r\n", rev);
+        if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+        if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+        rev = rev * 10 + pop; 
+    }
+    return rev;
+}
+
+#endif
 
 int main(int argc,char *argv[])
 {
