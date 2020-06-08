181beec09905f14982ce8babf104195fba79a334
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-06-08 20:37:16 +0800

    完善leetcode 2的code

--- ltc_2.c
+++ ltc_2.c
@@ -10,8 +10,34 @@ list_node_t* add_two_numbers(list_node_t* l1, list_node_t* l2)
 {
     if(NULL == l1 || NULL == l2)
         return NULL;
-    
-    
+
+    list_node_t *p = l1, *q = l2;
+    list_node_t *next = NULL,*head = NULL;
+
+    while(p || q)
+    {
+        next = calloc(1, sizeof(list_node_t));
+        if(NULL == next)
+            return NULL;
+
+        //产生node
+        next->val = ((NULL == p)?0:p->val) + ((NULL == q)?0:q->val);
+        next->next = NULL;
+        
+        if(NULL == head)
+            head = next;
+        else
+        {
+            next->next = head;
+            head->next = NULL;
+            head = next;
+        }
+            
+        p++;
+        q++;
+    }
+
+    return head;
 }
 
 int main(int argc,char *argv[])
