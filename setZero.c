11f52b3ab537f2cafd93c567baa2b9bab6dfe008
kk-1988 <40208167+kk-1988@users.noreply.github.com>
2022-08-23 20:45:53 +0800

    Update setZero.c

--- setZero.c
+++ setZero.c
@@ -26,15 +26,15 @@ int main(int argc, char *argv[])
 {
     int matrix[3][3];
     memset(matrix, 0, sizeof(matrix));
-    matrix[0][0] =
-    matrix[0][1] =
-    matrix[0][2] =
-    matrix[1][0] =
-    matrix[1][1] =
-    matrix[1][2] =
-    matrix[2][0] =
-    matrix[2][1] =
-    matrix[2][2] =
+    matrix[0][0] = 1;
+    matrix[0][1] = 1;
+    matrix[0][2] = 1;
+    matrix[1][0] = 1;
+    matrix[1][1] = 0;
+    matrix[1][2] = 1;
+    matrix[2][0] = 1;
+    matrix[2][1] = 1;
+    matrix[2][2] = 1;
 
     setZeroes();
     return 0;
