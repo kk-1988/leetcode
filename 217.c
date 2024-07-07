f9a693855b5021b775c2829d60d3a7c2dd4f21ac
xiaobo.kong <xiaobo.kong@aliyun.com>
2024-07-07 12:22:24 +0800

    Create 217.c
    初版提交

---
+++ 217.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool containsDuplicate(int* nums, int numsSize) {
+    int i,j;
+    //sort
+    for(i = 0;i < numsSize;i++) {
+        for(j = i;j < numsSize;j++) {
+            if(nums[j] > nums[i])
+            {
+                
+            }
+        }
+    }
+
+    return true;
+}
+
+int main(int argc, char *argv)
+{
+    
+}
