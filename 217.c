77838b4587389552f943d21239f6a9b423cc0534
xiaobo.kong <xiaobo.kong@aliyun.com>
2024-07-07 13:10:32 +0800

    Update 217.c
    排序版本

--- 217.c
+++ 217.c
@@ -2,22 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+int *cmp(int *a, int *b)
+{
+    return *a - *b;
+}
+
 bool containsDuplicate(int* nums, int numsSize) {
-    int i,j;
-    //sort
-    for(i = 0;i < numsSize;i++) {
-        for(j = i;j < numsSize;j++) {
-            if(nums[j] > nums[i])
-            {
-                
-            }
-        }
+    int i;
+    qsort(nums, numsSize, sizeof(int), cmp);
+
+    //judge
+    for(i = 0;i < numsSize - 1; i++) {
+        if(nums[i] == nums[i + 1])
+            return true;
     }
 
-    return true;
+    return false;
 }
 
 int main(int argc, char *argv)
 {
-    
+    int nums[] = {1,2,3,5,1,7,2,-1};
+    containsDuplicate(nums, sizeof(nums) / sizeof(nums[0]));
+
+    for(int i = 0;i < sizeof(nums) / sizeof(nums[0]);i++)
+    {
+        printf("%d ",nums[i]);
+    }
+    printf("\n");
 }
