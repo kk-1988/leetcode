e1a0f5366b9552ef5fdc0591bef7b6672ce0d2a0
kk-1988 <xiaobo.kong@aliyun.com>
2024-07-10 21:30:09 +0800

    Update 242.c
    pass

--- 242.c
+++ 242.c
@@ -6,8 +6,27 @@
 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
 */
 bool isAnagram(char* s, char* t) {
-    
-    return false;
+     unsigned short hashMapS[32] = { 0 };
+     unsigned short hashMapT[32] = { 0 };
+     int i;
+     if(strlen(s) != strlen(t))
+        return false;
+
+     for(i = 0;i < strlen(s);i++) {
+        hashMapS[s[i] - 'a']++;
+     }    
+
+     for(i = 0;i < strlen(t); i++) {
+        hashMapT[t[i] - 'a']++;
+     }
+
+     
+     for(i = 0;i < sizeof(hashMapS) / sizeof(hashMapS[0]);i++) {
+            if(hashMapS[i] != hashMapT[i])
+                return false;
+     }
+
+    return true;
 }
 
 int main(int argc, char *argv[]){
