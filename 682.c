df18356d5b7af81b69d869e99d81fb888f99efed
kk-1988 <xiaobo.kong@aliyun.com>
2024-08-02 10:18:31 +0800

    Create 682.c
    add 682.c

---
+++ 682.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int calPoints(char** operations, int operationsSize) {
+    
+}
+
+int main(int argc,char *argv[])
+{
+
+    return 0;
+}
