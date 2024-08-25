0b1ae552734117a1467172f8e7ed43b5681f273d
kk-1988 <xiaobo.kong@aliyun.com>
2024-08-25 21:15:43 +0800

    Update 387.c
    修改相关接口代码

--- 387.c
+++ 387.c
@@ -2,10 +2,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+int firstUniqChar(char* s) {
+    
+    
+    return -1;
+}
 
-
-int main(int argc, char *argv[]){
-
-
+int main(int argc, char *argv[])
+{
+    
     return 0;
 }
