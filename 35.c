2189cae75a35965e295e66a455031cd8edd82391
kk-1988 <xiaobo.kong@aliyun.com>
2024-09-16 13:14:30 +0800

    Update 35.c
    35 pass

--- 35.c
+++ 35.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while(left <= right) {
+       int mid = left + (right - left) / 2; 
+        if(target == nums[mid]) 
+            return mid;
+        else if(target < nums[mid]) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+
+        if(left > right)
+            return left;
+    }
+
+    return -1;
+}
+
 int main(int argc, char *argv[])
 {
 
