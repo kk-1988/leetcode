52e372b2cefda7cfca064d3ed94c7cd3356d69a6
kk-1988 <xiaobo.kong@aliyun.com>
2024-09-20 10:13:05 +0800

    Update 69.c
    69.c pass

--- 69.c
+++ 69.c
@@ -1,8 +1,27 @@
 //x的平方根
 #include <stdio.h>
 
+int mySqrt(int x) {
+    int left = 0,right = x;
+    int index = -1;
+
+    while(left <= right)
+    {
+        unsigned long mid = left + (right - left) / 2;
+        if(mid * mid > x) {
+            right = mid - 1;
+        }
+        else {
+            index = mid;
+            left = mid + 1;
+        }
+    }
+
+    return index;
+}
+
 int main(int argc, char *argv[])
 {
-
+    mySqrt(8);
     return 0;
 }
