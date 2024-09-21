eddbca8a0872a9352a8fff34c2c7b2f4364614cb
kk-1988 <xiaobo.kong@aliyun.com>
2024-09-21 21:40:42 +0800

    Create 278.c
    add 278

---
+++ 278.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    
+}
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
