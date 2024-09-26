215a68fcbebd6ffcb41e9f3762024c1ee976bfc1
kk-1988 <xiaobo.kong@aliyun.com>
2024-09-26 12:36:23 +0800

    Update 278.c
    278 pass

--- 278.c
+++ 278.c
@@ -4,7 +4,26 @@
 // bool isBadVersion(int version);
 
 int firstBadVersion(int n) {
-    
+    int left = 1, right = n;
+    int mid = 0;
+    while(left < right){
+        mid = left + (right - left) / 2;
+        if(false == isBadVersion(mid)) {
+            //ok
+            left = mid + 1;
+        } else {
+            //bad
+            right = mid;
+        }
+    }
+
+    if(isBadVersion(mid) == true)
+        return mid;
+
+    if(isBadVersion(left) == true)
+        return left;
+
+    return -1;
 }
 
 int main(int argc, char *argv[])
