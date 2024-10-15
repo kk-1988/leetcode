9ec282cc9f137d338e685991fa3f21ef5eaa89e8
kk-1988 <xiaobo.kong@aliyun.com>
2024-10-15 12:30:49 +0800

    Update 268.c
    补充268

---
+++ 268.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//是否需要先排序？
+int missingNumber(int* nums, int numsSize) {
+    int left = 0, right = numsSize;
+    int missNum;
+
+    //need sort？
+    while(left < numsSize) {
+        int mid = left + (right - left) / 2;
+        if(mid == )
+    }
+
+    return missNum;
+}
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
