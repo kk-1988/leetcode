3ba68f50ba93cbbd0726ee4f6079286bed270bdc
kk-1988 <40208167+kk-1988@users.noreply.github.com>
2024-12-23 12:43:33 +0800

    Update 169.c
    add 169.c

---
+++ 169.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int majorityElement(int* nums, int numsSize) {
+    
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    int nums[] = {};
+    majorElement();
+    return 0;
+}
