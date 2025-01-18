67b178438a3a6d1e7f8e5083009ea4b23ccd4b2a
xiaobo.kong <xiaobo.kong@aliyun.com>
2025-01-18 14:30:48 +0800

    Update 169.c
    补充169.c

--- 169.c
+++ 169.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 
-int majorityElement(int* nums, int numsSize) {
-    
+int cmp_int(const void *a, const void *b)
+{
+    return (*(int*)a - *(int*)b);
 }
 
+int majorityElement(int* nums, int numsSize) {
+   int temp;
+   int cnt;
+   int i;
+   
+    qsort(nums, numsSize, sizeof(int), cmp_int);
 
+    temp = nums[0];
+    cnt = 0; 
+    for(i = 0;i < numsSize; i++) {
+        if(nums[i] == temp) {
+            cnt++;
+            if(cnt > (numsSize / 2))
+                break;
+        } else {
+            temp = nums[i];
+            cnt = 1;
+        }
+    }
+
+    return temp;
+}
 
 int main(int argc, char *argv[])
 {
-    int nums[] = {};
-    majorElement();
+    // int nums[] = {};
+    // majorElement();
     return 0;
 }
